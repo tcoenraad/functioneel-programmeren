c1e6a4e8e658af0ca1ef5b3e365c99001ecb8aa6
tcoenraad <twan@coenraad.at>
2014-09-03 10:24:29 +0200

    Added first part of assignment 2 for week 1

---
+++ week1/opdracht2/opdracht.hs
@@ -0,0 +1,6 @@
+import FPPrac
+
+codeer :: Char -> Char
+codeer c | ord(c) <= ord('z') && ord(c) >= ord('a') = chr (((ord c - ord('a') + 3) `mod` 26) + ord('a'))
+         | ord(c) <= ord('Z') && ord(c) >= ord('A') = chr (((ord c - ord('a') + 3) `mod` 26) + ord('a'))
+         | otherwise = c
