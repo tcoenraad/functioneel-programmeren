e45a93de06cc876f448b8f76b5b9a89920cd02bd
tcoenraad <twan@coenraad.at>
2014-09-03 10:27:50 +0200

    Fixed bug for capital letters

--- week1/opdracht2/opdracht.hs
+++ week1/opdracht2/opdracht.hs
@@ -2,5 +2,5 @@ import FPPrac
 
 codeer :: Char -> Char
 codeer c | ord(c) <= ord('z') && ord(c) >= ord('a') = chr (((ord c - ord('a') + 3) `mod` 26) + ord('a'))
-         | ord(c) <= ord('Z') && ord(c) >= ord('A') = chr (((ord c - ord('a') + 3) `mod` 26) + ord('a'))
+         | ord(c) <= ord('Z') && ord(c) >= ord('A') = chr (((ord c - ord('A') + 3) `mod` 26) + ord('A'))
          | otherwise = c
