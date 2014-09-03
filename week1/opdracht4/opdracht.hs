b54e5fa057b4f5fd08940dc2645d832778534605
robertandringa <r.j.andringa@student.utwente.nl>
2014-09-03 11:51:28 +0200

    Added discr function

--- week1/opdracht4/opdracht.hs
+++ week1/opdracht4/opdracht.hs
@@ -1,12 +1,15 @@
 import FPPrac
 
+discr :: Number -> Number -> Number -> Number
+discr a b c = b^2 - 4*a*c
+
 wortel1 :: Number -> Number -> Number -> String
 
-wortel1 a b c | b^2 - 4*a*c < 0 = "discriminant negatief"
-              | otherwise = show ((-b - sqrt(b^2 - 4*a*c))/(2*a)) 
+wortel1 a b c | discr a b c < 0 = "discriminant negatief"
+              | otherwise = show ((-b - sqrt(discr a b c))/(2*a)) 
 
 			  
 wortel2 :: Number -> Number -> Number -> String
 			  
-wortel2 a b c | b^2 - 4*a*c < 0 = "discriminant negatief"
-              | otherwise = show ((-b + sqrt(b^2 - 4*a*c))/(2*a))
+wortel2 a b c | discr a b c < 0 = "discriminant negatief"
+              | otherwise = show ((-b + sqrt(discr a b c))/(2*a))
