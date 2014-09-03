15e03992de3ff568fd01756ddcd3a9b5fa40e4c2
tcoenraad <twan@coenraad.at>
2014-09-03 11:08:11 +0200

    Doing second assignment the Haskell way

--- week1/opdracht2/opdracht.hs
+++ week1/opdracht2/opdracht.hs
@@ -1,6 +1,8 @@
 import FPPrac
 
-codeer :: (Char, Number) -> Char
-codeer (c, shift) | ord(c) <= ord('z') && ord(c) >= ord('a') = chr (((ord c - ord('a') + shift) `mod` 26) + ord('a'))
-                  | ord(c) <= ord('Z') && ord(c) >= ord('A') = chr (((ord c - ord('A') + shift) `mod` 26) + ord('A'))
-                  | otherwise = c
+codeer :: Number -> Char -> Char
+codeer shift c | ord(c) <= ord('z') && ord(c) >= ord('a') = chr (((ord c - ord('a') + shift) `mod` 26) + ord('a'))
+               | ord(c) <= ord('Z') && ord(c) >= ord('A') = chr (((ord c - ord('A') + shift) `mod` 26) + ord('A'))
+               | otherwise = c
+
+-- map (codeer 3) "Morgenavond, 8 uur in Scheveningen"
