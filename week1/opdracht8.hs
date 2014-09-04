46236dd8377742d8622753d2a96e88ec2ec5d2d3
robertandringa <r.j.andringa@student.utwente.nl>
2014-09-04 15:45:58 +0200

    Finished assignment 8

--- week1/opdracht8.hs
+++ week1/opdracht8.hs
@@ -3,3 +3,10 @@ import FPPrac
 allEqual :: Eq(a) => [a] -> Bool
 allEqual [a] = True
 allEqual (x:xs) = x == head xs && allEqual xs
+
+isRR :: [Number] -> Bool
+isRR a = allEqual (calculateDifferences a)
+
+calculateDifferences :: [Number] -> [Number]
+calculateDifferences [a] = []
+calculateDifferences (x:xs) = [x - head xs] ++ calculateDifferences xs
