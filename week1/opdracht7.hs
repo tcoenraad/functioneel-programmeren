7706904ab4d919ad2e5390b81c1e34381e78a1f9
tcoenraad <twan@coenraad.at>
2014-09-04 15:19:30 +0200

    Finished assignment 7

--- week1/opdracht7.hs
+++ week1/opdracht7.hs
@@ -5,3 +5,6 @@ r s v = [s] ++ r (s+v) v
 
 r1 :: Number -> Number -> Number -> Number
 r1 s v n = (r s v) !! n
+
+r2 :: Number -> Number -> Number -> Number -> Number
+r2 s v i j = sum(drop i (take j (r s v)))
