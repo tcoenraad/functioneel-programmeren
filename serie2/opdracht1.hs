272152b6f1771272845d1ee6a92ff96d6474a8b3
tcoenraad <twan@coenraad.at>
2014-09-09 09:53:53 +0200

    Corrected foldr

--- serie2/opdracht1.hs
+++ serie2/opdracht1.hs
@@ -6,15 +6,13 @@ myfilter f (x:xs) | f x = x : myfilter f xs
                   | otherwise = myfilter f xs
 
 myfoldl :: (b -> a -> b) -> b -> [a] -> b
-myfoldl f s [n] = f s n
+myfoldl f s [] = s
 myfoldl f s (x:xs) = myfoldl f a xs
   where a = f s x
 
-myfoldr :: (b -> a -> b) -> b -> [a] -> b
-myfoldr f s [n] = f s n
-myfoldr f s x = myfoldr f a b
-  where a = f s (last x)
-        b = take (length x - 1) x
+myfoldr :: (a -> b -> b) -> b -> [a] -> b
+myfoldr f s [] = s
+myfoldr f s (x:xs) = f x (myfoldr f s xs)
 
 myzipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
 myzipWith f [] [] = []
