3e042b566ed04c40f3a7aa343ea4f8db96f2b288
tcoenraad <twan@coenraad.at>
2014-09-10 10:55:17 +0200

    Optimzed assignment 9

--- serie2/opdracht9.hs
+++ serie2/opdracht9.hs
@@ -2,9 +2,4 @@ import FPPrac
 import Data.List
 
 isort :: [Number] -> [Number]
-isort x = foldl (insert1) [] x 
-
-insert1 :: [Number] -> Number -> [Number]
-insert1 [] a = [a]
-insert1 (x:xs) a | a<=x = a : (x:xs)
-                 | otherwise = x: insert1 xs a
+isort x = foldl (flip insert) [] x 
