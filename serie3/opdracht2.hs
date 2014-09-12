18a688155c5b92749ea8473e783e822357322df2
robertandringa <r.j.andringa@student.utwente.nl>
2014-09-12 14:32:07 +0200

    fixed 2a for serie 3

--- serie3/opdracht2.hs
+++ serie3/opdracht2.hs
@@ -7,6 +7,8 @@ pp1a :: Tree1a -> RoseTree
 pp1a (Node1a i t1 t2) = RoseNode (show i) [(pp1a t1), (pp1a t2)]
 pp1a (Leaf1a i) = RoseNode (show i) []
 
-plusX :: Number -> Tree1a -> RoseTree
-plusX x(Node1a i t1 t2) = pp1a (Node1a (i+x) t1 t2)
-plusX x(Leaf1a i) = pp1a (Leaf1a(i+x))
+plusX :: Number -> Tree1a -> Tree1a
+plusX x (Node1a i t1 t2) = Node1a (i+x) (plusX x t1) (plusX x t2)
+plusX x (Leaf1a i) = (Leaf1a (i+x))
+
+--exampleTree2a = showTree(pp1a(plusX 4 (Node1a 1 (Leaf1a 35) (Leaf1a 36))))
