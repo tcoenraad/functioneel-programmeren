7ab6687dd338c4625df2c42311328e447533be17
tcoenraad <twan@coenraad.at>
2014-09-15 16:43:16 +0200

    Little improvements for assignment 6

--- serie3/opdracht6.hs
+++ serie3/opdracht6.hs
@@ -9,7 +9,7 @@ pp1a (Leaf1a i) = RoseNode (show i) []
 
 totDiepte :: Number -> Tree1a -> Tree1a
 totDiepte m (Node1a i t1 t2) | m/=0 = Node1a i (totDiepte (m-1) t1) (totDiepte (m-1) t2)
-                             | otherwise = totDiepte m (Leaf1a i)
+                             | otherwise = Leaf1a i
 totDiepte m (Leaf1a i) = Leaf1a i
 
 --showTree(pp1a(totDiepte 2 (Node1a 1 (Node1a 5 (Leaf1a 8) (Leaf1a 3)) (Leaf1a 3))))
