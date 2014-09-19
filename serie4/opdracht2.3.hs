44c4618da82dec03bfcb12148620b99ef53c1af5
tcoenraad <twan@coenraad.at>
2014-09-19 16:35:28 +0200

    Added parser for tokenized strings

--- serie4/opdracht2.3.hs
+++ serie4/opdracht2.3.hs
@@ -25,3 +25,21 @@ getNumber x = (takeWhile isDigit x, dropWhile isDigit x)
 getWord :: String -> (String, String)
 getWord x = (takeWhile isAlpha x, dropWhile isAlpha x)
 
+parseString :: String -> (BinTree String (Either String Number), String)
+parseString x = tokenizer $ parse
+
+data S = E | O
+
+parse :: S -> [ADT] -> (BinTree String (Either String Number), [ADT])
+parse E (LP:xs) = (Node op t1 t3, r4)
+                where 
+                (t1,r1) = parse E xs
+                ((Node op _ _), r2) = parse O r1
+                (t3,r3) = parse E r2
+                (_:r4) = r3
+
+parse E ((NUM x):xs) = (Leaf (Right x), xs)
+parse E ((VAR x):xs) = (Leaf (Left x), xs)
+parse E (x:xs) = error "Waa!"
+parse O ((OP x):xs) = (Node x (Leaf (Right 1)) (Leaf (Right 2)), xs)
+parse O (x:xs) = error "Wee!"
