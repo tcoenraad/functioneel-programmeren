aef67c0e4580d71b4714edb005a06847a71b5f29
tcoenraad <twan@coenraad.at>
2014-09-19 14:41:02 +0200

    Renamed 2 -> 2.1

--- serie4/opdracht2.1.hs
+++ serie4/opdracht2.1.hs
@@ -2,20 +2,19 @@ import FPPrac
 import FPPrac.Trees.ParseTree
 import Data.Char
 
-data BinTree a b = Leaf b | Node a (BinTree a b) (BinTree a b) | Unit deriving Show
+data BinTree a b = Leaf b | Node a (BinTree a b) (BinTree a b) deriving Show
 
-
-data S = E | O | P
+data S = E | O
 
 parse :: S -> String -> (BinTree Char Number, String)
-parse E (x:xs) | x == '(' && head r3 == ')' = (Node (head r1) t1 t3, r4)
+parse E (x:xs) | x == '(' && head r3 == ')' = (Node op t1 t3, r4)
                | isDigit x = (Leaf (read [x] :: Number), xs)
                | otherwise = error "Waa!"
         where 
-        (t1,r1) = parse E xs
+        (t1, r1) = parse E xs
         ((Node op _ _), r2) = parse O r1
-        (t3,r3) = parse E r2
-        (r:r4) = r3
+        (t3, r3) = parse E r2
+        (_:r4) = r3
 
 parse O (x:xs) | elem x "/^*+-" = (Node x (Leaf 1) (Leaf 2), xs)
                | otherwise = error "Woe!"
