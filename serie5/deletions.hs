08f52bab187f0089ffa1aea96c30a2237a8c7728
tcoenraad <twan@coenraad.at>
2014-09-23 22:31:10 +0200

    Fixed leftmostValue

--- serie5/deletions.hs
+++ serie5/deletions.hs
@@ -13,12 +13,8 @@ rbZetom (Tree Red Leaf) = RBNodeG RedG "" []
 rbZetom (Tree Black Leaf) = RBNodeG BlackG "" []
 rbZetom (Tree Grey Leaf) = RBNodeG GreyG "" []
 
-isLeaf :: RBTree -> Bool
-isLeaf (Tree _ Leaf) = True
-isLeaf _ = False
-
-leftmostValue :: RBTree -> Number
-leftmostValue (Tree c (Node n t1 t2)) | isLeaf t1 = n
-									  | otherwise = leftmostValue (t1)
+leftmostValue :: RBTree -> RBTree
+leftmostValue (Tree c (Node n t1 t2)) = leftmostValue (t1)
+leftmostValue (Tree c (Node n (Leaf t1) t2)) = t1
 
 -- leftmostValue(Tree Black (Node 3 (Tree Red (Node 1 (Tree Red Leaf) (Tree Red (Node 2 (Tree Red Leaf) (Tree Red Leaf))))) (Tree Red Leaf))) 
