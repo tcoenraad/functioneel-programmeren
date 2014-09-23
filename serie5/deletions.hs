bf711db316b9935ab7a08864e70f2b01c05bfd4c
tcoenraad <twan@coenraad.at>
2014-09-23 23:48:29 +0200

    Added r is red

--- serie5/deletions.hs
+++ serie5/deletions.hs
@@ -31,3 +31,9 @@ greyColourFlip (Tree Black (Node p (Tree Black (Node s (Tree Black r) (Tree Blac
 -- l is red
 greyColourFlip (Tree c1 (Node p (Tree Grey g) (Tree Black (Node s (Tree Red (Node l (Tree Black a) (Tree Black b))) (Tree c2 r))))) = (Tree c1 (Node l (Tree Black (Node p (Tree Black g) (Tree Black a))) (Tree Black (Node s (Tree Black b) (Tree c2 r)))))
 greyColourFlip (Tree c1 (Node p (Tree Black (Node s (Tree c2 r) (Tree Red (Node l (Tree Black b) (Tree Black a))))) (Tree Grey g))) = (Tree c1 (Node l (Tree Black (Node s (Tree c2 r) (Tree Black b))) (Tree Black (Node p (Tree Black a) (Tree Black g)))))
+-- p is red
+greyColourFlip (Tree Red (Node p (Tree Grey g) (Tree Black (Node s (Tree Black l) (Tree c r))))) = (Tree Grey (Node s (Tree Red (Node p (Tree Black g) (Tree Black l))) (Tree c r)))
+greyColourFlip (Tree Red (Node p (Tree Black (Node s (Tree c r) (Tree Black l))) (Tree Grey g))) = (Tree Grey (Node s (Tree c r) (Tree Red (Node p (Tree Black l) (Tree Black g)))))
+-- r is red
+greyColourFlip (Tree Black (Node p (Tree Grey g) (Tree Black (Node s (Tree Black l) (Tree Red r))))) = (Tree Grey (Node s (Tree Black (Node p (Tree Black g) (Tree Black l))) (Tree Black r)))
+greyColourFlip (Tree Black (Node p (Tree Black (Node s (Tree Red r) (Tree Black l))) (Tree Grey g))) = (Tree Grey (Node s (Tree Black r) (Tree Black (Node p (Tree Black l) (Tree Black g)))))
