faafb1a1e89f0fdb7a3c58dacdc2ea16ab60b52f
tcoenraad <twan@coenraad.at>
2014-09-24 00:33:39 +0200

    Anything else..

--- serie5/deletions.hs
+++ serie5/deletions.hs
@@ -40,6 +40,8 @@ greyColourFlip (Tree Black (Node p (Tree Black (Node s (Tree Red r) (Tree Black 
 -- s is red
 greyColourFlip (Tree Black (Node p (Tree Grey g) (Tree Red (Node s (Tree Black l) (Tree Black r))))) = (Tree Black (Node s (Tree Red (Node p (Tree Grey g) (Tree Black l))) (Tree Black r)))
 greyColourFlip (Tree Black (Node p (Tree Red (Node s (Tree Black r) (Tree Black l))) (Tree Grey g))) = (Tree Black (Node s (Tree Black r) (Tree Red (Node p (Tree Black l) (Tree Grey g)))))
+-- else
+greyColourFlip t = t
 
 greyRebalance :: RBTree -> RBTree
 greyRebalance (Tree c Leaf) = (Tree c Leaf)
