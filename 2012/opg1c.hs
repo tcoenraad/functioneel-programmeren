ac3a90e9f7a6a800a9ae8b46e611dad3c70b7398
Twan Coenraad <twan@coenraad.at>
2014-10-27 20:40:17 +0100

    Added matrix multiplication

---
+++ 2012/opg1c.hs
@@ -0,0 +1,23 @@
+import Data.List
+import Debug.Trace
+
+-- recursive
+matrixMultiplying :: [[Int]] -> [[Int]] -> [[Int]]
+matrixMultiplying [] [] = []
+matrixMultiplying as bs = matrixMultiplying' as (transpose bs)
+
+matrixMultiplying' :: [[Int]] -> [[Int]] -> [[Int]]
+matrixMultiplying' [] _ = []
+matrixMultiplying' (a:as) b = calcRow a b : matrixMultiplying' as b
+
+calcRow :: [Int] -> [[Int]] -> [Int]
+calcRow _ [] = []
+calcRow a (b:bs) = sum (zipWith (*) a b) : calcRow a bs
+
+-- higher order
+matrixMultiply :: [[Int]] -> [[Int]] -> [[Int]]
+matrixMultiply as bs = map (\a -> map (\b -> sum (zipWith (*) a b)) (transpose bs)) as
+
+-- list comprehension
+matrixMultiplier :: [[Int]] -> [[Int]] -> [[Int]]
+matrixMultiplier a b = [[sum $ zipWith (*) ar bc | bc <- transpose b] | ar <- a]
