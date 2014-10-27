5f05a85eb279381f9a5b4bb93ab982a1abb0d13b
Twan Coenraad <twan@coenraad.at>
2014-10-27 22:09:06 +0100

    Added toNF

---
+++ 2012/opg2c.hs
@@ -0,0 +1,15 @@
+data OA = Add | Mul deriving Show
+data OB = EQu | GTh | LTh deriving Show
+data ExprA = Const Int
+           | Var Char
+           | OpA OA ExprA ExprA
+           | If ExprB ExprA ExprA
+            deriving Show
+
+data ExprB = OpB OB ExprA ExprA deriving Show
+
+toNF :: ExprA -> ExprA
+toNF (OpA Add (OpA Add expr1 expr2) (expr3)) = OpA Add (toNF expr1) (OpA Add (toNF expr2) (toNF expr3))
+toNF (OpA Mul (OpA Mul expr1 expr2) (expr3)) = OpA Mul (toNF expr1) (OpA Mul (toNF expr2) (toNF expr3))
+toNF (OpA Mul expr1 (OpA Add expr2 expr3)) = OpA Add (OpA Mul (toNF expr1) (toNF expr2)) (OpA Mul (toNF expr1) (toNF expr3))
+toNF expr = expr
