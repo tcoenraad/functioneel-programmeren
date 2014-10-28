ce47cce75c873543ef980524503dcb73023a8d99
Twan Coenraad <twan@coenraad.at>
2014-10-28 13:59:52 +0100

    Added Pascals triangle

---
+++ 2014/opg1c.hs
@@ -0,0 +1,10 @@
+pascal :: Int -> [[Int]]
+pascal n = [(pascal' k) | k <- [0..n]] 
+
+pascal' :: Int -> [Int]
+pascal' 0 = [1]
+pascal' 1 = [1, 1]
+pascal' n = [1] ++ pascal'' (pascal' (n-1)) ++ [1]
+
+pascal'' :: [Int] -> [Int]
+pascal'' n = zipWith (+) n (tail n)
