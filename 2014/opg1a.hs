7405c3aa0c3f50d8300b51cdd5f6e51c4bb315a9
Twan Coenraad <twan@coenraad.at>
2014-11-04 23:35:03 +0100

    Added generalization of question

--- 2014/opg1a.hs
+++ 2014/opg1a.hs
@@ -5,3 +5,6 @@ list35 k = [x | x <- [1..k-1], x `mod` 3 == 0 || x `mod` 5 == 0]
 
 add35 :: Int -> Int
 add35 k = sum $ list35 k
+
+add :: [Int] -> Int -> Int
+add ks n = sum $ filter (\x -> or $ map (\y -> x `mod` y == 0) [2..n]) ks
