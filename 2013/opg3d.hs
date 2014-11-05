4d437c153fdba1fc77a69bf803b0c743ba47c2c7
Twan Coenraad <twan@coenraad.at>
2014-11-05 12:51:02 +0100

    Remove unneeded $ sign

--- 2013/opg3d.hs
+++ 2013/opg3d.hs
@@ -3,7 +3,7 @@ module Exercise where
 data Tree = Node Int [Tree] | Leaf Int deriving Show
 
 padnaar :: Int -> Tree -> [Int]
-padnaar i t | (length ps) > 0 = head $ ps
+padnaar i t | (length ps) > 0 = head ps
             | otherwise = error "No path can be found"
               where ps = filter (elem i) (allPaths t)
 
