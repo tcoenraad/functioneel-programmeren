2c4911719f799e968152f5c42a1431040b6e44af
Twan Coenraad <twan@coenraad.at>
2014-12-09 22:30:12 -0500

    Removed confusing part on example for path finding

--- practica/serie6/find-paths.hs
+++ practica/serie6/find-paths.hs
@@ -1,9 +1,6 @@
 -- http://stackoverflow.com/questions/11168238/haskell-generating-all-paths-between-nodes
 
 import Prelude
-import Data.Graph
-
-g = buildG (1,5) [(1,2),(1,6),(2,3),(3,4),(4,5),(2,5)]
 
 g!1 = [2,6]
 g!2 = [3,5]
