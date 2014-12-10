6d7d59498823f4b15f656e8f837515a69c04931e
Twan Coenraad <twan@coenraad.at>
2014-12-09 22:30:29 -0500

    Added given files

---
+++ practica/serie5/AftekenBoom.hs
@@ -0,0 +1,52 @@
+aftekenBoom = Tree Black (Node 15
+                        (Tree Black (Node 7
+                            (Tree Red (Node 3
+                                (Tree Black (Node 1
+                                    (Tree Red (Node 1
+                                        (Tree Black Leaf)
+                                        (Tree Black Leaf)
+                                    ))
+                                    (Tree Red (Node 2
+                                        (Tree Black Leaf)
+                                        (Tree Black Leaf)
+                                    ))
+                                ))
+                                (Tree Black (Node 5
+                                    (Tree Red (Node 4
+                                        (Tree Black Leaf)
+                                        (Tree Black Leaf)
+                                    ))
+                                    (Tree Red (Node 6
+                                        (Tree Black Leaf)
+                                        (Tree Black Leaf)
+                                    ))
+                                ))
+                            ))
+                            (Tree Black (Node 10
+                                (Tree Red (Node 8
+                                    (Tree Black Leaf)
+                                    (Tree Black Leaf)
+                                ))
+                                (Tree Red (Node 12
+                                    (Tree Black Leaf)
+                                    (Tree Black Leaf)
+                                ))
+                            ))
+                        ))
+                        (Tree Black (Node 25
+                            (Tree Black (Node 20
+                                (Tree Black Leaf)
+                                (Tree Black Leaf)
+                            ))
+                            (Tree Red (Node 30
+                                (Tree Black (Node 28
+                                    (Tree Black Leaf)
+                                    (Tree Black Leaf)
+                                ))
+                                (Tree Black (Node 60
+                                    (Tree Black Leaf)
+                                    (Tree Black Leaf)
+                                ))
+                            ))
+                        ))
+                    )
