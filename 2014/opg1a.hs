f314c2cc053d00027797f776877070fbc3ba6bd3
Twan Coenraad <twan@coenraad.at>
2015-01-20 09:47:29 +0100

    Fixed error in `add ks n` exercise and added alternative solution

--- 2014/opg1a.hs
+++ 2014/opg1a.hs
@@ -7,4 +7,7 @@ add35 :: Int -> Int
 add35 k = sum $ list35 k
 
 add :: [Int] -> Int -> Int
-add ks n = sum $ filter (\x -> or $ map (\y -> x `mod` y == 0) [2..n]) ks
+add ks n = sum $ filter (\x -> any (\y -> x `mod` y == 0) ks) [0..n-1]
+
+add' :: [Int] -> Int -> Int
+add' ks n = sum [n | n <- [0..n-1], any (\k -> n`mod`k == 0) ks]
